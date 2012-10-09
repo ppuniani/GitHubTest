8279796d3e42f8af4ba8829e37e62946e5d75da8
ppuniani <pankaj.puniani@am.sony.com>
2012-10-08 18:27:51 -0700

    some more updates

--- snippets/snippets.rb
+++ snippets/snippets.rb
@@ -1,8 +1,8 @@
 require 'ruble'
 
 snippet "Sample Snippet" do |snip|
-  snip.trigger = "sample"
-  snip.expansion = "sample_snippet"
+  snip.trigger = "sample123"
+  snip.expansion = "sample_snippet123"
 end
 
 # Use Commands > Bundle Development > Insert Bundle Section > Snippet
